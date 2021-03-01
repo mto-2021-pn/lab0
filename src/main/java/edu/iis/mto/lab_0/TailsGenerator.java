b3c7d4390adf1a3ee62db30daa6cc46b6f402343
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-03-01 18:50:50 +0200

    zmiana fin tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        // stworzona nowa lista i dodano po koleji stringi do tej nowej listy, ostatni string jest pusty
-        List<String> returnList = new ArrayList<String>();
+        // stworzona nowa lista slow i dodano po koleji stringi do tej nowej listy, ostatni string jest pusty
+        List<String> returnListOfStrings = new ArrayList<String>();
         for(int i=0; i<=value.length(); i++){
-            returnList.add(value.substring(i, value.length()));
+            returnListOfStrings.add(value.substring(i, value.length()));
         }
 
-        return returnList;
+        return returnListOfStrings;
     }
 
 }
