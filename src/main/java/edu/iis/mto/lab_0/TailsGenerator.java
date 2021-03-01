1049e18e0dc2cef5b6d1f327f83011a50a264599
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-03-01 18:38:04 +0200

    dodanie komentarzy do funkcji tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,12 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
+        // stworzona nowa lista i dodano po koleji stringi do tej nowej listy, ostatni string jest pusty
         List<String> returnList = new ArrayList<String>();
-
         for(int i=0; i<=value.length(); i++){
             returnList.add(value.substring(i, value.length()));
         }
-        System.out.println(returnList);
 
         return returnList;
     }
