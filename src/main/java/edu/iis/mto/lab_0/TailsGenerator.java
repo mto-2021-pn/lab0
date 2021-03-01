41ba1832ca4be48f9c24b5d9425c95d9e5011d8b
monikaciezka <223959@edu.p.lodz.pl>
2021-03-01 17:44:39 +0100

    tails function body

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        List<String> resultList = new ArrayList<>();
+        int length = value.length();
+        for(int counter = length; counter >= 0; counter--){
+            resultList.add(value.substring(0, counter));
+
+        }
+        return resultList;
     }
 
 }
