ce2c5079574c0cd940d60ff068b6a146a2d6d040
Jan Oleksik <222529@edu.p.lodz.pl>
2021-03-01 17:29:45 +0100

    Rozwiazanie 1 testu

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -8,6 +8,8 @@ public class TailsGenerator {
     public List<String> tails(String value) {
         List<String> list = new ArrayList<>(value.length());
         list.add(value);
+        for (int i = 0; i < value.length(); i++)
+            list.add(value);
         return list;
     }
 
